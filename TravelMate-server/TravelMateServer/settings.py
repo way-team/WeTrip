64d04fc0c8bda4da4e5da61829bebdee615f3123
Unknown <javierherraizolivas@gmail.com>
2019-03-20 19:19:25 +0100

    Added PostgreSQL as DB
    User: travelmate
    password: tRaV€lMAt€_$tRoNg
    db_name: travelmate

--- TravelMate-server/TravelMateServer/settings.py
+++ TravelMate-server/TravelMateServer/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'TravelMateServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travelmate',
+        'USER': 'travelmate',
+        'PASSWORD': 'tRaV€lMAt€_$tRoNg',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
