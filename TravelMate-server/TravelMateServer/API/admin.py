d3ade9e69fcc25461d0c9a9c89642765d4b94d16
Javier <javier21roga@gmail.com>
2019-03-23 19:14:21 +0100

    [Data models] Adding the data application models.
    S
    W
    We are added all data tables for our application in the models.py.
    To use this, is neccesary the following command:
    "python .\manage.py migrate" on Windows.
    "python3 ./manage.py migrate" on Linux.

--- TravelMate-server/TravelMateServer/API/admin.py
+++ TravelMate-server/TravelMateServer/API/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Language, Interest, Rate, Invitation, Message, Application, Global, Trip
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Language)
+admin.site.register(Interest)
+admin.site.register(Rate)
+admin.site.register(Invitation)
+admin.site.register(Message)
+admin.site.register(Application)
+admin.site.register(Global)
+admin.site.register(Trip)
