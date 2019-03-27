dbd0077cac0b2842a8d7d320595ca9d03c45ce6e
Rubentoro <rubentoro1997@gmail.com>
2019-03-27 18:47:04 +0100

    Created method to get friends and pending friends

--- TravelMate-server/TravelMateServer/API/views.py
+++ TravelMate-server/TravelMateServer/API/views.py
@@ -3,10 +3,11 @@ from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .models import UserProfile
+from .models import UserProfile, Invitation
 from django.contrib.auth.models import User
 from rest_framework import authentication, generics
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 
 
 class GetUserView(APIView):
@@ -31,3 +32,22 @@ class UserList(APIView):
         username = kwargs.get('username')
         userProfile = User.objects.get(username=username).userprofile
         return Response(UserProfileSerializer(userProfile, many=False).data)
+        
+
+class GetFriendsView(APIView):
+    def get(self, request):
+        """
+        Method to get the friends of the logged user and his pending friends 
+        """
+        key = request.data.get('token', '')
+        tk = get_object_or_404(Token, key=key)
+        user = tk.user
+
+        try:
+            friends = Invitation.objects.filter(Q(sender = user) | Q(receiver=user) , status="A")
+            pendingFriends = Invitation.objects.filter(receiver=user , status="P")
+        except:
+            friends = []
+            pendingFriends = []
+        
+        return Response({"friends": friends, "pendingFriends": pendingFriends})
