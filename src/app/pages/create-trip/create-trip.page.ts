f0d3a5cfd1e588ba3c8623d79b0d850bd0586310
Unknown <javierherraizolivas@gmail.com>
2019-03-28 08:26:11 +0100

    [Front-end]: Added navagibility

--- src/app/pages/create-trip/create-trip.page.ts
+++ src/app/pages/create-trip/create-trip.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { City } from '../../app.data.model';
 import { DataManagement } from '../../services/dataManagement';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AlertController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-trip',
@@ -16,12 +16,13 @@ export class CreateTripPage implements OnInit {
   start_date: Date;
   end_date: Date;
   trip_type: string = 'PUBLIC';
-  image: string;
+  image;
   city: Number;
   error: string;
   cities: City[];
 
   constructor(
+    public navCtrl: NavController,
     public dm: DataManagement,
     private formBuilder: FormBuilder,
     public alertCtrl: AlertController
@@ -49,7 +50,9 @@ export class CreateTripPage implements OnInit {
         this.image,
         this.city
       )
-      .then(data => {})
+      .then(data => {
+        this.navCtrl.navigateRoot('/trips');
+      })
       .catch(error => {
         this.alertCtrl
           .create({
