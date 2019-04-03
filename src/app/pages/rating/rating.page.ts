49f607337646e4e7be82fb1731c97c4d13637851
CarmenJara <carjaralv@alum.us.es>
2019-04-03 12:37:19 +0200

    [Front-end] rating page
    not finished

---
+++ src/app/pages/rating/rating.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
+import { ActivatedRoute  } from '@angular/router';
+import { UserProfile } from 'src/app/app.data.model';
+import { DataManagement } from 'src/app/services/dataManagement';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.page.html',
+  styleUrls: ['./rating.page.scss'],
+})
+export class RatingPage implements OnInit {
+
+  public user: UserProfile;
+  private username: string;
+
+
+  constructor(
+      private dm: DataManagement,
+    private cookieService: CookieService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.username = this.activatedRoute.snapshot.paramMap.get('username');
+   this.getUser(this.username);
+  }
+
+  ngOnInit() {
+
+  }
+    private getUser(username: string) {
+    const token = this.cookieService.get('token');
+    this.dm.getUserBy(username, token).then((res: UserProfile) => {
+      this.user = res;
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+}
