097c4573ce8f90ad963dcc23e1120daf5a2df97e
CarmenJara <carjaralv@alum.us.es>
2019-04-05 13:15:19 +0200

    [Front-end]: paypal
    pay-pal count working and payment. It is necessary to make logic  to the server, that way the client changes to premium.

--- src/app/pages/premium/premium.page.ts
+++ src/app/pages/premium/premium.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit} from '@angular/core';
 import { NavController, LoadingController, ToastController } from '@ionic/angular';
+import * as $ from "jquery";
+
+declare let paypal: any;
 
 @Component({
   selector: 'app-premium',
@@ -14,25 +17,49 @@ export class PremiumPage implements OnInit {
    public loadingCtrl: LoadingController) { }
 
   ngOnInit() {
-  }
+
+    }
+
   async payment() {
     const loader = await this.loadingCtrl.create({
       duration: 2000
-    });
+    });}
 
-    loader.present();
-    loader.onWillDismiss().then(async l => {
-      const toast = await this.toastCtrl.create({
-        showCloseButton: true,
-        cssClass: 'bg-profile',
-        message: 'This feature is not implemented yet!',
-        duration: 3000,
-         position: 'bottom'
-      });
+   loadExternalScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script')
+      scriptElement.src = scriptUrl
+      scriptElement.onload = resolve
+      document.body.appendChild(scriptElement)
+  });}
+ngAfterViewInit(): void {
+    this.loadExternalScript("https://www.paypalobjects.com/api/checkout.js").then(() => {
+      paypal.Button.render({
+        env: 'sandbox',
+        client: {
+          production: '',
+          sandbox: 'AddLPh79rdzGB5QdmTWhN0coPnuTCg9_1hEkeyKv6rZtoK-m4MIaq3syIe5sCRmndpu9WAt-gpwLWBs9'
+        },
+        commit: true,
+        payment: function (data, actions) {
+          return actions.payment.create({
+            payment: {
+              transactions: [
+                {
+                  amount: { total: '10.00', currency: 'EUR' }
+                }
+              ]
+            }
+          })
+        },
+        onAuthorize: function(data, actions) {
+          return actions.payment.execute().then(function(payment) {
+            // TODO
 
-      toast.present();
-      this.navCtrl.navigateForward('/discover');
+          })
+        }
+      }, '#paypal-button');
     });
   }
 
-}
+  }
