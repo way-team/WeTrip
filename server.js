90424b3ee1d2b519d6f8c033951132bbf8bc1a80
Unknown <javierherraizolivas@gmail.com>
2019-04-06 14:45:57 +0200

    Trying to fix heroku problems when refreshing

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+var history = require('connect-history-api-fallback');
+
 var express = require('express');
 var path = require('path')
 var app = express();
+app.use(history());
 app.use(express.static(path.resolve(__dirname, "www")));
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function() {
- console.log("listening to Port", app.get("port"));
+app.listen(app.get('port'), function () {
+    console.log("listening to Port", app.get("port"));
 });
