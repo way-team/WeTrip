29ce96b2d557b28dc51e49d352158ed7b51d2369
davigldom <davigldom@gmail.com>
2019-05-01 21:13:07 +0100

    [Back-end]: Changes in "Export user data" method

--- TravelMate-server/TravelMateServer/API/views.py
+++ TravelMate-server/TravelMateServer/API/views.py
@@ -16,7 +16,15 @@ from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from collections import namedtuple
 from django.contrib.auth.hashers import make_password
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from string import Template
+import smtplib
+import re
 import json
+from io import BytesIO
+#from reportlab.pdfgen import canvas
 
 
 def get_user_by_token(request):
@@ -1217,15 +1225,67 @@ def send_mail(subject, body, email, attachment):
 
     server.quit()
 
-
+# En desarrollo
 class ExportUserData(APIView):
     """
     Export the user's data and send it to his email
     """
+    def header(self, pdf):
+        #Utilizamos el archivo logo_django.png que está guardado en la carpeta media/imagenes
+        archivo_imagen = settings.MEDIA_ROOT+'/imagenes/logo_django.png'
+        #Definimos el tamaño de la imagen a cargar y las coordenadas correspondientes
+        pdf.drawImage(archivo_imagen, 40, 750, 120, 90, preserveAspectRatio=True)
+        #Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
+        pdf.setFont("Helvetica", 16)
+        #Dibujamos una cadena en la ubicación X,Y especificada
+        pdf.drawString(230, 790, u"PYTHON PIURA")
+        pdf.setFont("Helvetica", 14)
+        pdf.drawString(200, 770, u"REPORTE DE PERSONAS")
+
+    def table(self, pdf, y):
+        #Creamos una tupla de encabezados para neustra tabla
+        encabezados = ('DNI', 'Nombre', 'Apellido Paterno', 'Apellido Materno')
+        #Creamos una lista de tuplas que van a contener a las personas
+        detalles = [(persona.dni, persona.nombre, persona.apellido_paterno, persona.apellido_materno) for persona in Persona.objects.all()]
+        #Establecemos el tamaño de cada una de las columnas de la tabla
+        detalle_orden = Table([encabezados] + detalles, colWidths=[2 * cm, 5 * cm, 5 * cm, 5 * cm])
+        #Aplicamos estilos a las celdas de la tabla
+        detalle_orden.setStyle(TableStyle(
+            [
+                #La primera fila(encabezados) va a estar centrada
+                ('ALIGN',(0,0),(3,0),'CENTER'),
+                #Los bordes de todas las celdas serán de color negro y con un grosor de 1
+                ('GRID', (0, 0), (-1, -1), 1, colors.black), 
+                #El tamaño de las letras de cada una de las celdas será de 10
+                ('FONTSIZE', (0, 0), (-1, -1), 10),
+            ]
+        ))
+        #Establecemos el tamaño de la hoja que ocupará la tabla 
+        detalle_orden.wrapOn(pdf, 800, 600)
+        #Definimos la coordenada donde se dibujará la tabla
+        detalle_orden.drawOn(pdf, 60, y)
+
+    def get(self, request, *args, **kwargs):
+        #Indicamos el tipo de contenido a devolver, en este caso un pdf
+        response = HttpResponse(content_type='application/pdf')
+        #La clase io.BytesIO permite tratar un array de bytes como un fichero binario, se utiliza como almacenamiento temporal
+        buffer = BytesIO()
+        #Canvas nos permite hacer el reporte con coordenadas X y Y
+        pdf = canvas.Canvas(buffer)
+        #Llamo al método cabecera donde están definidos los datos que aparecen en la cabecera del reporte.
+        self.cabecera(pdf)
+        y = 600
+        self.tabla(pdf, y)
+        #Con show page hacemos un corte de página para pasar a la siguiente
+        pdf.showPage()
+        pdf.save()
+        pdf = buffer.getvalue()
+        buffer.close()
+        response.write(pdf)
+        return response
+
+"""
     def post(self, request):
-        """
-        POST method
-        """
         user_id = request.data.get("user_id", "")
         email = request.data.get("email", "")
 
@@ -1269,6 +1329,7 @@ class ExportUserData(APIView):
         send_mail("Exported data", "This is the exported data of " + user.username, email, "exported_data_" + user.username + ".pdf")
 
         os.remove("exported_data_" + user.username + ".pdf")
+"""
 
 
 def backendWakeUp(request):
